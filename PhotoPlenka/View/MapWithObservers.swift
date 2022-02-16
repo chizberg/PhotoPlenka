b9b203c96724e3abd4baaabe7bd417a6d7577ccd
Alex <43221673+alexxingplus@users.noreply.github.com>
2022-02-17 02:28:18 +0300

    basic annimations i don't like the implementation

--- PhotoPlenka/View/MapWithObservers.swift
+++ PhotoPlenka/View/MapWithObservers.swift
@@ -8,6 +8,11 @@
 import MapKit
 
 final class MapWithObservers: MKMapView, MapPublisher {
+    private enum Style {
+        static let annotationAnimationDuration: TimeInterval = 0.2
+        static let superSmallTransform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    }
+
     // храним множество слабых ссылок
     private(set) var observers = NSHashTable<MapObserver>.weakObjects()
 
@@ -37,9 +42,22 @@ final class MapWithObservers: MKMapView, MapPublisher {
         annotationsDidChange()
     }
 
+    // почему-то анимация не воспроизводится, если её перенести в MapController
     override func removeAnnotations(_ annotations: [MKAnnotation]) {
-        super.removeAnnotations(annotations)
-        annotationsDidChange()
+        let group = DispatchGroup()
+        for annotation in visibleAnnotations {
+            guard let annotationView = view(for: annotation) else { continue }
+            group.enter()
+            DispatchQueue.main.async(group: group) {
+                UIView.animate(withDuration: Style.annotationAnimationDuration, animations: {
+                    annotationView.transform = Style.superSmallTransform
+                }, completion: { _ in group.leave() })
+            }
+        }
+        group.notify(queue: .main) {
+            super.removeAnnotations(annotations)
+            self.annotationsDidChange()
+        }
     }
 
     override func removeAnnotation(_ annotation: MKAnnotation) {
