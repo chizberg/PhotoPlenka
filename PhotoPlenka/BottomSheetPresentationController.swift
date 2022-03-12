9e3eefe2388d0945d2d016b545c3a5a9e412ee08
Alex Sherstnev <43221673+alexxingplus@users.noreply.github.com>
2022-03-13 01:19:59 +0300

    Revert "add header gesture recognizer"

--- PhotoPlenka/BottomSheetPresentationController.swift
+++ PhotoPlenka/BottomSheetPresentationController.swift
@@ -8,32 +8,9 @@
 import Foundation
 import UIKit
 
-protocol ScrollableViewController {
-    var scrollView: UIScrollView { get }
-    var header: UIView { get }
-}
-
-final class BottomSheetPresentationController: UIPresentationController,
-                                               UIGestureRecognizerDelegate,
-                                               UIScrollViewDelegate {
-    enum Mode {
-        case opened
-        case collapsed
-    }
-    private weak var scrollView: UIScrollView?
-    private weak var headerView: UIView?
-    private let fractions: [Double]
-    private var mode: Mode = .collapsed
+final class BottomSheetPresentationController: UIPresentationController {
     private var initialFrame: CGRect = .zero
-    private var scrollPanGesture: UIPanGestureRecognizer!
-    private var headerPanGesture: UIPanGestureRecognizer!
-    private var scrollViewOffset: CGFloat = 0
-    private unowned let contentViewController: UIViewController
-
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
-                           shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
-    }
+    private let fractions: [Double]
 
     override var shouldPresentInFullscreen: Bool {
         false
@@ -42,9 +19,9 @@ final class BottomSheetPresentationController: UIPresentationController,
     override var frameOfPresentedViewInContainerView: CGRect {
         guard let containerViewBounds = presentingViewController.view?.bounds,
               let firstFraction = fractions.first else {
-                  assertionFailure("Presenting view must exist")
-                  return .zero
-              }
+            assertionFailure("Presenting view must exist")
+            return .zero
+        }
 
         let y = calculateYAxis(fraction: firstFraction)
         let height = calculateHeightBy(yAxisValue: y)
@@ -60,14 +37,12 @@ final class BottomSheetPresentationController: UIPresentationController,
     init(
         fractions: [Double],
         presentedViewController: UIViewController,
-        presenting presentingViewController: UIViewController?,
-        contentViewController: UIViewController
+        presenting presentingViewController: UIViewController?
     ) {
         guard !fractions.isEmpty else {
             fatalError("Must have at leat one element")
         }
         self.fractions = fractions.sorted(by: <)
-        self.contentViewController = contentViewController
         super.init(
             presentedViewController: presentedViewController,
             presenting: presentingViewController
@@ -79,35 +54,26 @@ final class BottomSheetPresentationController: UIPresentationController,
 
 extension BottomSheetPresentationController {
     private func configurePanGesture() {
-        scrollPanGesture = UIPanGestureRecognizer(target: self, action: #selector(scrollGestureHandler(_:)))
-        scrollPanGesture.delegate = self
-        headerPanGesture = UIPanGestureRecognizer(target: self, action: #selector(headerGestureHandler))
-        headerView?.addGestureRecognizer(headerPanGesture)
-        scrollView?.addGestureRecognizer(scrollPanGesture)
-    }
-
-    @objc
-    func headerGestureHandler(_ gesture: UIPanGestureRecognizer) {
-        if gesture.state == .began {
-            scrollViewOffset = scrollView!.contentOffset.y
-            initialFrame = containerView!.frame
-            scrollView?.isScrollEnabled = false
-        } else if gesture.state == .changed {
-            scrollView?.setContentOffset(.init(x: 0, y: scrollViewOffset), animated: false)
-
-            let transition = gesture.translation(in: presentedView).y
-            panChangedHandler(offset: transition,
-                              view: containerView!,
-                              initialFrame: initialFrame)
-        } else if gesture.state == .ended || gesture.state == .cancelled {
-            panEndedHandler(gesture)
-        }
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(swipeGesture(_:)))
+        presentedViewController.view?.addGestureRecognizer(swipeGesture)
     }
 }
 
 // MARK: - Helpers
 
 extension BottomSheetPresentationController {
+    private func findMostCloseFraction(_ value: Double) -> Double {
+        var minDistance = 1.0
+        var closestFractionIndex = 0
+        for (index, fraction) in fractions.enumerated() {
+            if abs(fraction - value) < minDistance {
+                minDistance = abs(fraction - value)
+                closestFractionIndex = index
+            }
+        }
+        return fractions[closestFractionIndex]
+    }
+
     private func calculateYAxis(fraction: Double) -> Double {
         let presentingBounds = self.presentingViewController.view.bounds
         return presentingBounds.height - presentingBounds.height * fraction
@@ -123,12 +89,6 @@ extension BottomSheetPresentationController {
 extension BottomSheetPresentationController {
     override func presentationTransitionWillBegin() {
         super.presentationTransitionWillBegin()
-        if let scrollable = contentViewController as? ScrollableViewController {
-            scrollView = scrollable.scrollView
-            headerView = scrollable.header
-            scrollView?.delegate = self
-            scrollView?.isScrollEnabled = false
-        }
         containerView?.frame = frameOfPresentedViewInContainerView
     }
 
@@ -156,101 +116,41 @@ extension BottomSheetPresentationController {
 
 extension BottomSheetPresentationController {
     @objc
-    func scrollGestureHandler(_ gesture: UIPanGestureRecognizer) {
-        guard scrollView!.contentOffset.y <= 0 || mode == .collapsed else { return }
-        guard let containerFrame = containerView?.frame else { return }
+    func swipeGesture(_ gesture: UIPanGestureRecognizer) {
+        guard let containerFrame = containerView?.frame else {
+            return
+        }
 
         if gesture.state == .began {
             initialFrame = containerFrame
         } else if gesture.state == .changed {
-            scrollView?.isScrollEnabled = false
             let transition = gesture.translation(in: presentedView).y
-            panChangedHandler(offset: transition,
-                              view: containerView!,
-                              initialFrame: initialFrame)
-        } else if gesture.state == .ended || gesture.state == .cancelled {
-            panEndedHandler(gesture)
-        }
-    }
-
-    private func panChangedHandler(offset: CGFloat, view: UIView, initialFrame: CGRect) {
-        view.frame = .init(
-            x: 0,
-            y: initialFrame.minY + offset,
-            width: initialFrame.width,
-            height: initialFrame.height - offset
-        )
-    }
-
-    private func panEndedHandler(_ gesture: UIPanGestureRecognizer) {
-        // Calculate how much spaces container view takes
-        let progressValue = 1 -
-        (containerView!.frame.minY / presentingViewController.view.bounds.height)
-        var closestFraction = findClosestValue(progressValue, from: fractions)
-
-        updateClosestFractionIfNeeded(velocityByY: gesture.velocity(in: presentedView).y,
-                                      closestValue: &closestFraction,
-                                      fractions: fractions)
-        updateModeIfNeeded(currentFraction: closestFraction, fractions: fractions)
-        enableScrollIfNeeded(mode: mode)
-
-        let presentingBounds = presentingViewController.view.bounds
-        let targetY = calculateYAxis(fraction: closestFraction)
-        let targetHeight = calculateHeightBy(yAxisValue: targetY)
-
-        UIView.animate(withDuration: 0.2, animations: {
-            self.containerView?.frame = CGRect(
+            containerView?.frame = .init(
                 x: 0,
-                y: targetY,
-                width: presentingBounds.width,
-                height: targetHeight
+                y: initialFrame.minY + transition,
+                width: initialFrame.width,
+                height: initialFrame.height - transition
             )
-            self.containerView?.layoutIfNeeded()
-        })
-    }
 
-    private func updateModeIfNeeded(currentFraction: CGFloat, fractions: [Double]) {
-        mode = currentFraction == fractions.last! ? .opened : .collapsed
-    }
+        } else if gesture.state == .ended {
+            let progressLocation = 1 -
+                (containerFrame.minY / presentingViewController.view.bounds.height)
 
-    private func enableScrollIfNeeded(mode: Mode) {
-        scrollView?.isScrollEnabled = mode == .opened
-    }
+            UIView.animate(withDuration: 0.2, animations: {
+                let closestFraction = self.findMostCloseFraction(progressLocation)
 
-    // If velocity value is too high,
-    // the distanse between first and last touches doesn't matter
-    // we can just skip to the next mode
-    private func updateClosestFractionIfNeeded(velocityByY value: CGFloat,
-                                               closestValue: inout Double,
-                                               fractions: [Double]) {
-        guard let indexWhere = fractions.firstIndex(of: closestValue) else {
-            fatalError("Incorrect fractions")
-        }
+                let presentingBounds = self.presentingViewController.view.bounds
+                let targetY = self.calculateYAxis(fraction: closestFraction)
+                let targetHeight = self.calculateHeightBy(yAxisValue: targetY)
 
-        if value <= -Self.verticalVelocityThreashold {
-            let indexAfter = fractions.index(after: indexWhere)
-            if indexAfter < fractions.count {
-                closestValue = fractions[indexAfter]
-            }
-        } else if value >= Self.verticalVelocityThreashold {
-            let indexBefore = fractions.index(before: indexWhere)
-            if indexBefore >= 0 {
-                closestValue = fractions[indexBefore]
-            }
+                self.containerView?.frame = CGRect(
+                    x: 0,
+                    y: targetY,
+                    width: presentingBounds.width,
+                    height: targetHeight
+                )
+                self.containerView?.layoutIfNeeded()
+            })
         }
     }
-
-    static let verticalVelocityThreashold: CGFloat = 1000
-}
-
-fileprivate func findClosestValue(_ value: Double, from values: [Double]) -> Double {
-    var minDistance = 1.0
-    var closestFractionIndex = 0
-    for (index, fraction) in values.enumerated() {
-        if abs(fraction - value) < minDistance {
-            minDistance = abs(fraction - value)
-            closestFractionIndex = index
-        }
-    }
-    return values[closestFractionIndex]
 }
