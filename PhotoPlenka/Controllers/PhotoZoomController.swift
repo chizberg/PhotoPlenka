5a184cb24e2a468db9e0c3fc8b8659e48817c08e
Alex <43221673+alexxingplus@users.noreply.github.com>
2022-04-17 03:23:15 +0300

    🔨 actions now it's possible to get a route to a certain photo via Yandex maps, Google maps, Apple maps or 2GIS also sharing is a bit different
    anyway, now ALL BUTTONS ARE FINALLY WORKING 🎉
    we're close to beta testing

--- PhotoPlenka/Controllers/PhotoZoomController.swift
+++ PhotoPlenka/Controllers/PhotoZoomController.swift
@@ -13,19 +13,13 @@ final class PhotoZoomController: UIViewController {
         static let buttonColor: UIColor = .white
         static let sideInset: CGFloat = 16
         static let buttonSize: CGSize = .init(width: 40, height: 40)
-        static let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .bold)
-        static let closeIcon: UIImage = .init(systemName: "xmark", withConfiguration: config)!
-        static let shareIcon: UIImage = .init(
-            systemName: "square.and.arrow.up",
-            withConfiguration: config
-        )!
         static let animationDuration: TimeInterval = 0.25
     }
 
     private let image: UIImage
 
     private let closeButton = RoundButton(type: .close)
-    private let shareButton = RoundButton(type: .share)
+    private let saveButton = RoundButton(type: .download)
     private let imageView: ZoomableImageView = ZoomableImageView()
 
     init(image: UIImage){
@@ -42,15 +36,15 @@ final class PhotoZoomController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = Style.backgroundColor
         closeButton.translatesAutoresizingMaskIntoConstraints = false
-        shareButton.translatesAutoresizingMaskIntoConstraints = false
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = image
         view.addSubview(imageView)
         view.addSubview(closeButton)
-        view.addSubview(shareButton)
+        view.addSubview(saveButton)
         applyConstraints()
 
         closeButton.addTarget(self, action: #selector(close), for: .touchUpInside)
-        shareButton.addTarget(self, action: #selector(share), for: .touchUpInside)
+        saveButton.addTarget(self, action: #selector(save), for: .touchUpInside)
 
         let tap = UITapGestureRecognizer(target: self, action: #selector(changeControls))
         imageView.addGestureRecognizer(tap)
@@ -74,10 +68,10 @@ final class PhotoZoomController: UIViewController {
         ])
 
         NSLayoutConstraint.activate([
-            shareButton.widthAnchor.constraint(equalToConstant: Style.buttonSize.width),
-            shareButton.heightAnchor.constraint(equalToConstant: Style.buttonSize.height),
-            shareButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Style.sideInset),
-            shareButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Style.sideInset)
+            saveButton.widthAnchor.constraint(equalToConstant: Style.buttonSize.width),
+            saveButton.heightAnchor.constraint(equalToConstant: Style.buttonSize.height),
+            saveButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Style.sideInset),
+            saveButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Style.sideInset)
         ])
     }
 
@@ -85,42 +79,45 @@ final class PhotoZoomController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
-    @objc private func share(){
-        let imagesToShare = [image]
-        let shareSheet = UIActivityViewController(activityItems: imagesToShare, applicationActivities: nil)
-        shareSheet.popoverPresentationController?.sourceView = shareButton
-        present(shareSheet, animated: true, completion: nil)
+    @objc private func save(){
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted(_:didFinishSavingWithError:contextInfo:)), nil)
     }
 
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+
+
     @objc private func changeControls() {
-        let isHidden = shareButton.isHidden
+        let isHidden = saveButton.isHidden
         setControlState(isHidden: !isHidden, animated: true)
     }
 
     func setControlState(isHidden: Bool, animated: Bool) {
         guard animated else {
-            shareButton.isHidden = isHidden
+            saveButton.isHidden = isHidden
             closeButton.isHidden = isHidden
             return
         }
         switch isHidden {
         case true:
-            shareButton.alpha = 1
+            saveButton.alpha = 1
             closeButton.alpha = 1
             UIView.animate(withDuration: Style.animationDuration, animations: { [unowned self] in
-                shareButton.alpha = 0
+                saveButton.alpha = 0
                 closeButton.alpha = 0
             }, completion: { [unowned self] _ in
-                shareButton.isHidden = true
+                saveButton.isHidden = true
                 closeButton.isHidden = true
             })
         case false:
-            shareButton.alpha = 0
+            saveButton.alpha = 0
             closeButton.alpha = 0
-            shareButton.isHidden = false
+            saveButton.isHidden = false
             closeButton.isHidden = false
             UIView.animate(withDuration: Style.animationDuration) { [unowned self] in
-                shareButton.alpha = 1
+                saveButton.alpha = 1
                 closeButton.alpha = 1
             }
         }
